954ae0741fe6d1e71979a4a905a80af324382242
kassi-bertrand <bertran.billy@gmail.com>
2024-12-31 00:53:44 -0600

    Removed "09"

--- 09-Build a Transformer from scratch/train-char-shakespeare.sh
+++
@@ -1,8 +0,0 @@
-# Install build tools
-apt-get -qq update && apt-get -qq install --no-install-recommends -y build-essential
-
-# Python packages
-pip install -r requirements.txt
-
-# Kick off the training process. NOTE: I was in a distributed environment
-torchrun --standalone --nnodes=1 train.py config/train_shakespeare_char.py
